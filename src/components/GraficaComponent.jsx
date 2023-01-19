e0bd24d6234d5240abe9e935bd41fe54c9a99ed4
oguerrero <oscarguerrero242@gmail.com>
2023-01-19 13:50:49 -0600

    add dynamic doughnut chart

--- src/components/GraficaComponent.jsx
+++ src/components/GraficaComponent.jsx
@@ -1,19 +1,32 @@
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js/auto'
+import { useEffect, useState } from 'react'
 import { Doughnut } from 'react-chartjs-2'
+import { useResolution } from '../context/ResolutionContext'
 
 ChartJS.register(ArcElement, Tooltip, Legend)
 
 export default function GraficaComponent() {
+  const { resolutions, getResolutions } = useResolution()
+
+  useEffect(() => {
+    getResolutions()
+  }, [])
+
+  let completos = 0
+  let incompletos = 0
+
+  resolutions.map((resolution) => {
+    if (resolution.done) completos++
+    else incompletos++
+  })
+
   const data = {
     labels: ['Completados', 'Incompletos'],
     datasets: [
       {
         label: 'Total',
-        data: [55, 23],
-        backgroundColor: [
-          'rgb(67, 56, 202)',
-          'rgb(225, 29, 72)'
-        ],
+        data: [completos, incompletos],
+        backgroundColor: ['rgb(67, 56, 202)', 'rgb(225, 29, 72)'],
         borderColor: 'black',
         borderWidth: 2
       }
