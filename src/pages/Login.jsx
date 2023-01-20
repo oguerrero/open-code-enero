c4bb0d977ddce5e8b36f9c61f0494ba3411ee518
oguerrero <oscarguerrero242@gmail.com>
2023-01-20 10:55:44 -0600

    change url

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -27,7 +27,7 @@ export default function Login() {
     const { data, error } = await supabase.auth.signInWithOtp({
       email,
       options: {
-        emailRedirectTo: 'https://resolutionsui.onrender.com/login'
+        emailRedirectTo: 'https://resolutionsui.onrender.com/'
       }
     })
     toast.info('Tu enlace se ha enviado al correo')
@@ -37,7 +37,7 @@ export default function Login() {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider,
       options: {
-        emailRedirectTo: 'https://resolutionsui.onrender.com/login'
+        emailRedirectTo: 'https://resolutionsui.onrender.com/'
       }
     })
   }
